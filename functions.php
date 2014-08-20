3a109f353a111c3521ca251fd06587b9dedab152
Greg Rickaby <gregrickaby@gmail.com>
2014-08-20 10:13:38 -0500

    add live reload. remove default css so we can pass in compiled/minified css. add JS

--- functions.php
+++ functions.php
@@ -7,11 +7,33 @@ define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.1.2' );
 
-//* Enqueue Google Fonts
-add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
-function genesis_sample_google_fonts() {
+//* Enqueue Styles and Scripts
+add_action( 'wp_enqueue_scripts', 'genesis_sample_scripts' );
+function genesis_sample_scripts() {
 
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	$minnified = '.min';
+
+	//* Should we load minified scripts? Also enqueue live reload to allow for extensionless reloading with 'grunt watch'
+	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG == true ) {
+
+		$minnified = '';
+		wp_enqueue_script( 'live-reload', '//localhost:35729/livereload.js', array(), CHILD_THEME_VERSION, true );
+
+	}
+
+	//* Add Google Fonts
+	wp_register_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'google-fonts' );
+
+	//* Remove default CSS
+	wp_dequeue_style( 'genesis-sample-theme' );
+
+	//* Add compiled CSS
+	wp_register_style( 'genesis-sample-styles', get_stylesheet_directory_uri() . '/style' . $minnified . '.css' );
+	wp_enqueue_style( 'genesis-sample-styles' );
+
+	//* Add compiled JS
+	wp_enqueue_script( 'genesis-sample-scripts', get_template_directory_uri() . '/js/project' . $minnified . '.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 
 }
 
