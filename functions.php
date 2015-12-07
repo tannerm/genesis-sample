681077728cba8865aadeabf537ae99322780aecb
Greg Rickaby <gregrickaby@gmail.com>
2015-12-07 09:48:29 -0600

    bring styles current with genesis 2.2.2. fixes #5

--- functions.php
+++ functions.php
@@ -5,18 +5,18 @@ include_once( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.1.2' );
+define( 'CHILD_THEME_VERSION', '2.2.2' );
 
-//* Enqueue Styles and Scripts
+//* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_scripts' );
 function genesis_sample_scripts() {
 
-	$minnified = '.min';
+	$suffix = '.min';
 
-	//* Should we load minified scripts? Also enqueue live reload to allow for extensionless reloading with 'grunt watch'.
+	//* Should we load minified scripts? Also enqueue live reload to allow for extensionless reloading with 'grunt watch'
 	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG == true ) {
 
-		$minnified = '';
+		$suffix = '';
 		wp_enqueue_script( 'live-reload', '//localhost:35729/livereload.js', array(), CHILD_THEME_VERSION, true );
 
 	}
@@ -25,20 +25,24 @@ function genesis_sample_scripts() {
 	wp_register_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'google-fonts' );
 
-	//* Remove default CSS
-	wp_dequeue_style( 'genesis-sample-theme' );
+	//* Remove default stylesheet
+	wp_register_style( 'genesis-sample-theme' );
 
-	//* Add compiled CSS
-	wp_register_style( 'genesis-sample-styles', get_stylesheet_directory_uri() . '/style' . $minnified . '.css', array(), CHILD_THEME_VERSION );
-	wp_enqueue_style( 'genesis-sample-styles' );
+	//* Add compiled stylesheet
+	wp_register_style( 'genesis-sample-theme', get_stylesheet_directory_uri() . '/style' . $minnified . '.css', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'genesis-sample-theme' );
 
 	//* Add compiled JS
 	wp_enqueue_script( 'genesis-sample-scripts', get_stylesheet_directory_uri() . '/js/project' . $minnified . '.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+
 
 }
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+//* Add Accessibility support
+add_theme_support( 'genesis-accessibility', array( 'headings', 'drop-down-menu',  'search-form', 'skip-links', 'rems' ) );
 
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
