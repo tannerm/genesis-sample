e00c5753b061a8f71f040973b57877c861915249
Greg Rickaby <gregrickaby@gmail.com>
2016-04-25 10:13:23 -0500

    ha, use the right function name prefix

--- functions.php
+++ functions.php
@@ -94,8 +94,8 @@ remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
 
 //* Reduce the secondary navigation menu to one level depth
-add_filter( 'wp_nav_menu_args', 'atmosphere_secondary_menu_args' );
-function atmosphere_secondary_menu_args( $args ) {
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
 
 	if ( 'secondary' != $args['theme_location'] ) {
 		return $args;
