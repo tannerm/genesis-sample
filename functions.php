88f6724c9bc08f6cdad62feca86e92591cc298d8
Greg Rickaby <gregrickaby@gmail.com>
2016-04-25 10:13:39 -0500

    add svg-icons.def to wp_head

--- functions.php
+++ functions.php
@@ -106,3 +106,16 @@ function genesis_sample_secondary_menu_args( $args ) {
 	return $args;
 
 }
+
+//* Add SVG definitions to <head>.
+add_action( 'wp_head', 'genesis_sample_include_svg_icons', 999 );
+function genesis_sample_include_svg_icons() {
+
+	// Define SVG sprite file.
+	$svg_icons = get_template_directory() . 'images/svg-icons.svg';
+
+	// If it exsists, include it.
+	if ( file_exists( $svg_icons ) ) {
+		require_once( $svg_icons );
+	}
+}
